8809e4b48af847aec691157469db97b8cfeb85e2
Massamba Niang <bemax.ng@gmail.com>
2023-12-13 14:20:18 +0000

    Build For Prod !

---
+++ public/build/assets/FormIndications-9edc05da.js
@@ -0,0 +1,1 @@
+import{r as e}from"./InputLabel-abf86e56.js";import{o as s,c as t,f as a,u as r,a as o}from"./app-589b6ab5.js";const c={class:"flex items-center gap-2"},n=o("p",{class:"text-xs text-red-600"},"Champs Obligatoires",-1),l={__name:"FormIndications",setup(_){return(i,m)=>(s(),t("div",c,[a(r(e),{class:"text-red-600 h-1.5 w-1.5"}),n]))}};export{l as _};
--- public/build/assets/FormIndications-b5d9b5bb.js
+++
@@ -1,1 +0,0 @@
-import{r as e}from"./InputLabel-29a6a3dc.js";import{o as s,c as t,f as a,u as r,a as o}from"./app-97f24108.js";const c={class:"flex items-center gap-2"},n=o("p",{class:"text-xs text-red-600"},"Champs Obligatoires",-1),l={__name:"FormIndications",setup(_){return(i,m)=>(s(),t("div",c,[a(r(e),{class:"text-red-600 h-1.5 w-1.5"}),n]))}};export{l as _};

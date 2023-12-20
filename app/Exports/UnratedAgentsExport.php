0ee63b69d533931d3198959fb1ac16596a163837
Massamba Niang <bemax.ng@gmail.com>
2023-12-20 08:40:39 +0000

    Fix Downloads !

--- app/Exports/UnratedAgentsExport.php
+++ app/Exports/UnratedAgentsExport.php
@@ -46,7 +46,7 @@ class UnratedAgentsExport implements FromCollection, ShouldAutoSize, WithHeading
         return [
             $row->user_display_name,
             $row->user_matricule ?? '',
-            $row->org ? $row->org_name : 'Direction Inconnu',
+            $row->org ? $row->org->org_name : 'Direction Inconnu',
         ];
     }
 
